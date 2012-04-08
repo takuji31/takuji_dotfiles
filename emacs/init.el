5bd62f5a459d92cee294c5232548b31aeeb5d8a0
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-09 08:39:25 +0900

    Emacsの設定ある程度終わった

--- emacs/init.el
+++ emacs/init.el
@@ -89,3 +89,117 @@
 (setq-default tab-width 4)
 ;;タブなんてなかったんや
 (setq-default indent-tabs-mode nil)
+
+;;カラーの設定
+(when (require 'color-theme nil t)
+  (color-theme-initialize)
+  (when (require 'color-theme-wombat nil t)
+    (color-theme-wombat)))
+;;ハイライト
+(global-hl-line-mode t)
+
+;;括弧の強調表示
+(setq show-paren-delay 0)
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
+
+;;1行目がシェバングなら実行権限つけて保存
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;;Anything.el
+(when (require 'anything nil t)
+  (setq
+   ;; 候補を表示するまでの時間
+   anything-idle-delay 0.3
+   ;; 再描画までの時間
+   anything-input-idle-delay 0.2
+   ;; 候補の最大数
+   anything-candidate-number-limit 100
+   ;; 体感速度を速くする
+   anything-quick-update t
+   ;; ショートカットをアルファベットに
+   anything-enable-shorcuts 'alphabet)
+
+  (when (require 'anything-config nil t)
+    (setq anything-su-or-sudo "sudo"))
+
+  (require 'anything-match-plugin nil t)
+
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (require 'anything-migemo nil t))
+
+  (when (require 'anything-complete nil t)
+    (anything-lisp-complete-symbol-set-timer 150))
+
+  (require 'anything-show-completion nil t)
+
+  (when (require 'auto-install nil t)
+    (require 'anything-auto-install nil t))
+
+  (when (require 'descbinds-anything nil t)
+    (descbinds-anything-install)))
+
+;;Auto Complete Mode
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories
+               "~/.emacs.d/elisp/ac-dict")
+  ;; FIXME C-TABだとうまくきかないっぽい、M-TABだとWindowsで使えない、さぁどうする
+  (define-key ac-mode-map (kbd "C-TAB") 'auto-complete)
+  (ac-config-default))
+
+;;color-moccur
+(when (require 'color-moccur nil t)
+  (global-set-key (kbd "M-o") 'occur-by-moccur)
+  (setq moccur-split-word t)
+  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+  (add-to-list 'dmoccur-exclusion-mask "^#.+#$")
+  (when (and (executable-find "cmigemo")
+             (require 'migemo nil t))
+    (setq moccur-use-migemo t))
+
+  (require 'moccur-edit nil t))
+
+;;wgrep
+(require 'wgrep nil t)
+
+;;undohist
+(when (require 'undohist nil t)
+  (undohist-initialize))
+
+;;undo-tree
+(when (require 'undo-tree nil t)
+  (global-undo-tree-mode))
+
+;; point-undo
+(when (require 'point-undo nil t)
+  (global-set-key (kbd "M-[") 'point-undo)
+  (global-set-key (kbd "M-]") 'point-redo))
+
+;;cperl-modeの設定
+(defalias 'perl-mode 'cperl-mode)
+;;; インデント
+(setq cperl-indent-level 4
+      cperl-continued-statement-offset 4
+      cperl-brace-offset -4
+      cperl-label-offset -4
+      cperl-indent-parens-as-block t
+      cperl-close-paren-offset -4
+      cperl-tab-always-indent t
+      cperl-highlight-variables-indiscriminately t)
+
+;;yaml-mode
+(when (require 'yaml-mode nil t)
+  (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode)))
+
+(defun perl-completion-hook ()
+  (when (require 'perl-completion nil t)
+    (perl-completion-mode t)
+    (when (require 'auto-complete nil t)
+      (auto-complete-mode t)
+      (make-variable-buffer-local 'ac-sources)
+      (setq ac-sources '(ac-source-perl-completion)))))
+(add-hook 'cperl-mode-hook 'perl-completion-hook)
+
