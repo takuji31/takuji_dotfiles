dd5b16aefc3d616eb126d39746db66642b3633e7
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-10 18:09:23 +0900

    とりあえずフォントの設定追加したけど怪しいのでコメントアウトしとく

---
+++ emacs/conf/cocoa-emacs-config.el
@@ -0,0 +1,3 @@
+;;font-set
+;;(set-face-attribute 'default nil :family "Ricty" :height 180)
+;;(set-fontset-font "fontset-default" 'japanese-jisx0208 '("Ricty" . "iso10646-*"))
--- emacs/init.el
+++ emacs/init.el
@@ -145,6 +145,10 @@
                "~/.emacs.d/elisp/ac-dict")
   ;; FIXME C-TABだとうまくきかないっぽい、M-TABだとWindowsで使えない、さぁどうする
   (define-key ac-mode-map (kbd "C-TAB") 'auto-complete)
+  ;;auto-complete.elでだけ別フォントを使う
+  ;;(set-face-font 'ac-candidate-face "VL Gothic 18")
+  ;;(set-face-font 'ac-selection-face "VL Gothic 18")
+  ;;(setq popup-use-optimized-column-computation nil)
   (ac-config-default))
 
 ;;color-moccur
