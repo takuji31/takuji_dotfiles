3a8fa6d9f62c46683239fa110c1b42fdc18128dd
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-12 11:55:03 +0900

    フォントをRictyの20に設定

--- emacs/conf/cocoa-emacs-config.el
+++ emacs/conf/cocoa-emacs-config.el
@@ -1,3 +1,13 @@
-;;font-set
-;;(set-face-attribute 'default nil :family "Ricty" :height 180)
-;;(set-fontset-font "fontset-default" 'japanese-jisx0208 '("Ricty" . "iso10646-*"))
+;; フォントの設定
+(let* ((size 20)
+           (asciifont "Ricty") ; ASCII fonts
+           (jpfont "Ricty") ; Japanese fonts
+           (h (* size 10))
+           (fontspec (font-spec :family asciifont))
+           (jp-fontspec (font-spec :family jpfont)))
+      (set-face-attribute 'default nil :family asciifont :height h)
+      (set-fontset-font nil 'japanese-jisx0213.2004-1 jp-fontspec)
+      (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+      (set-fontset-font nil 'katakana-jisx0201 jp-fontspec)
+      (set-fontset-font nil '(#x0080 . #x024F) fontspec) 
+      (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
