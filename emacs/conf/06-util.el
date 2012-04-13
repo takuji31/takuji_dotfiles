b7d15aff9b90edd2a8f5ec855461379e6601ec77
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-14 02:24:00 +0900

    text translator追加

--- emacs/conf/06-util.el
+++ emacs/conf/06-util.el
@@ -3,3 +3,6 @@
 ;; open-junk-file
 (require 'open-junk-file)
 (setq open-junk-file-format "~/junk/%Y-%m/%Y-%m-%d-%H%M%S.")
+
+(require 'text-translator)
+(setq text-translator-auto-selection-func 'text-translator-translate-by-auto-selection-enja)
