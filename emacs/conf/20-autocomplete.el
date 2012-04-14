1e078beb7790f1dc6bc72d20096693ce79821c79
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-14 22:25:38 +0900

    自動補完をC-TABからM-TABにしていらないコメント削除

--- emacs/conf/20-autocomplete.el
+++ emacs/conf/20-autocomplete.el
@@ -3,10 +3,6 @@
   (add-to-list 'ac-dictionary-directories
                "~/.emacs.d/elisp/ac-dict")
   ;; FIXME C-TABだとうまくきかないっぽい、M-TABだとWindowsで使えない、さぁどうする
-  (define-key ac-mode-map (kbd "C-TAB") 'auto-complete)
-  ;;auto-complete.elでだけ別フォントを使う
-  ;;(set-face-font 'ac-candidate-face "VL Gothic 18")
-  ;;(set-face-font 'ac-selection-face "VL Gothic 18")
-  ;;(setq popup-use-optimized-column-computation nil)
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   (ac-config-default))
 
