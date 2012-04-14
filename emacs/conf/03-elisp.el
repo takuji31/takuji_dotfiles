592dd5edec73e5309aa857c894c3b35ec1414b14
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-15 00:29:08 +0900

    再びauto-async-byte-compileを有効に

--- emacs/conf/03-elisp.el
+++ emacs/conf/03-elisp.el
@@ -7,3 +7,8 @@
 (setq eldoc-idle-delay 0.2)
 
 (require 'lispxmp)
+
+(when (require 'auto-async-byte-compile nil t)
+  ;; 自動コンパイルを無効にするファイル名の正規表現
+  (setq auto-async-byte-compile-exclude-files-regexp "(init.el|/conf/)")
+  (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
