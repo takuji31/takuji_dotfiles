f4f4ed600d7c80e5657a249b16a16542970ed91a
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-14 22:46:03 +0900

    なんか設定おかしいので消す

--- emacs/conf/30-anything.el
+++ emacs/conf/30-anything.el
@@ -1,16 +1,16 @@
 ;;Anything.el
 (require 'anything-startup)
-(setq
-   ;; 候補を表示するまでの時間
-   anything-idle-delay 0.3
-   ;; 再描画までの時間
-   anything-input-idle-delay 0.2
-   ;; 候補の最大数
-   anything-candidate-number-limit 100
-   ;; 体感速度を速くする
-   anything-quick-update t
-   ;; ショートカットをアルファベットに
-   anything-enable-shorcuts 'alphabet)
+;;(setq
+;;   ;; 候補を表示するまでの時間
+;;   anything-idle-delay 0.3
+;;   ;; 再描画までの時間
+;;   anything-input-idle-delay 0.2
+;;   ;; 候補の最大数
+;;   anything-candidate-number-limit 100
+;;   ;; 体感速度を速くする
+;;   anything-quick-update t
+;;   ;; ショートカットをアルファベットに
+;;   anything-enable-shorcuts 'alphabet)
 (setq anything-su-or-sudo "sudo")
 (anything-lisp-complete-symbol-set-timer 150)
 
