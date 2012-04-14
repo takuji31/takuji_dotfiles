36b37fa2765eaf38ae614d8383c257413eb77ef8
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-14 13:46:31 +0900

    anything.el本気モード

--- emacs/conf/04-operation.el
+++ emacs/conf/04-operation.el
@@ -1,14 +1,6 @@
 ;; find-file-at-point
 (ffap-bindings)
 
-;; バッファ切り替え強化
-(iswitchb-mode 1)
-(setq read-buffer-function 'iswitchb-read-buffer)
-;; 正規表現を使えるように
-(setq iswitchb-regexp t)
-;; バッファー作成時に確認をしない
-(setq iswitchb-prompt-newbuffer nil)
-
 ;; redo機能の追加
 (when (require 'redo+ nil t)
   (global-set-key (kbd "C-.") 'redo))
--- emacs/conf/30-anything.el
+++ emacs/conf/30-anything.el
@@ -1,6 +1,6 @@
 ;;Anything.el
-(when (require 'anything nil t)
-  (setq
+(require 'anything-startup)
+(setq
    ;; 候補を表示するまでの時間
    anything-idle-delay 0.3
    ;; 再描画までの時間
@@ -11,24 +11,14 @@
    anything-quick-update t
    ;; ショートカットをアルファベットに
    anything-enable-shorcuts 'alphabet)
+(setq anything-su-or-sudo "sudo")
+(anything-lisp-complete-symbol-set-timer 150)
 
-  (when (require 'anything-config nil t)
-    (setq anything-su-or-sudo "sudo"))
+;; anything-for-filesをC-;で起動してやる
+(global-set-key (kbd "C-;") 'anything-for-files)
+;; M-yをanything-show-kill-ringに置き換え
+(global-set-key (kbd "M-y") 'anything-show-kill-ring)
 
-  (require 'anything-match-plugin nil t)
-
-  (when (and (executable-find "cmigemo")
-             (require 'migemo nil t))
-    (require 'anything-migemo nil t))
-
-  (when (require 'anything-complete nil t)
-    (anything-lisp-complete-symbol-set-timer 150))
-
-  (require 'anything-show-completion nil t)
-
-  (when (require 'auto-install nil t)
-    (require 'anything-auto-install nil t))
-
-  (when (require 'descbinds-anything nil t)
-    (descbinds-anything-install)))
+;; descbinds-anythingを画面分割で表示
+(setq descbinds-anything-window-style 'split-window)
 
